1fb0ba7b08a6f060a25d06146ea58699d907d058
wmsj100 <wmsj100@hotmail.com>
2020-07-14 12:57:38 +0000

    Add base template add error template

--- app.py
+++ app.py
@@ -68,13 +68,22 @@ def forge():
 @app.route('/index')
 def index():
 
-	user = User.query.first()
 	movies = Movies.query.all()
-	return render_template('index.html', user=user, movies=movies)
+	return render_template('index.html', movies=movies)
 
 @app.route('/user/<user>')
 def user(user):
 	return 'User is %s' % escape(user)
+
+
+@app.context_processor
+def inject_user():
+	user = User.query.first()
+	return dict(user=user)
+
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template('404.html'), 404
 
 @app.route('/test')
 def test1():
