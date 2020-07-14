86ac2385ffd142f73d26d4ec1a6ab9041676a52e
wmsj100 <wmsj100@hotmail.com>
2020-07-14 12:06:55 +0000

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -10,13 +10,36 @@
 
 """
 from flask import Flask, escape, url_for, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+import click
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////' + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
-@app.route('/')
-@app.route('/home')
-@app.route('/index')
-def hello():
+class User(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(20))
+
+class Movies(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(60))
+	year = db.Column(db.String(4))
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+	if drop:
+		click.echo('Drop all data')
+		db.drop_all()
+	db.create_all()
+	click.echo('Initialized database.')
+
+@app.cli.command()
+def forge():
+	db.create_all()
 	name = 'Grey Li'
 	movies = [
 			{'title': 'My Neighbor Totoro', 'year': '1988'},
@@ -30,8 +53,24 @@ def hello():
 			{'title': 'WALL-E', 'year': '2008'},
 			{'title': 'The Pork of Music', 'year': '2012'},
 			]
+	user = User(name=name)
+	db.session.add(user)
 
-	return render_template('index.html', name=name, movies=movies)
+	for m in movies:
+		movie = Movies(title=m['title'], year=m['year'])
+		db.session.add(movie)
+	
+	db.session.commit()
+	click.echo('Install template data OK!')
+
+@app.route('/')
+@app.route('/home')
+@app.route('/index')
+def index():
+
+	user = User.query.first()
+	movies = Movies.query.all()
+	return render_template('index.html', user=user, movies=movies)
 
 @app.route('/user/<user>')
 def user(user):
@@ -39,7 +78,7 @@ def user(user):
 
 @app.route('/test')
 def test1():
-	print(url_for('hello'))
+	print(url_for('index'))
 	print(url_for('user', user='wmsj100'))
 	print(url_for('test1'))
 	return 'Test page'
