2b4c4cdba382f9f5d8cda8cb328aff65e2feeaf6
wmsj100 <wmsj100@hotmail.com>
2020-07-14 11:19:06 +0000

    add index page

--- app.py
+++ app.py
@@ -9,7 +9,7 @@
 """
 
 """
-from flask import Flask, escape, url_for
+from flask import Flask, escape, url_for, render_template
 
 app = Flask(__name__)
 
@@ -17,7 +17,21 @@ app = Flask(__name__)
 @app.route('/home')
 @app.route('/index')
 def hello():
-	return 'Welcome to test my page'
+	name = 'Grey Li'
+	movies = [
+			{'title': 'My Neighbor Totoro', 'year': '1988'},
+			{'title': 'Dead Poets Society', 'year': '1989'},
+			{'title': 'A Perfect World', 'year': '1993'},
+			{'title': 'Leon', 'year': '1994'},
+			{'title': 'Mahjong', 'year': '1996'},
+			{'title': 'Swallowtail Butterfly', 'year': '1996'},
+			{'title': 'King of Comedy', 'year': '1999'},
+			{'title': 'Devils on the Doorstep', 'year': '1999'},
+			{'title': 'WALL-E', 'year': '2008'},
+			{'title': 'The Pork of Music', 'year': '2012'},
+			]
+
+	return render_template('index.html', name=name, movies=movies)
 
 @app.route('/user/<user>')
 def user(user):
